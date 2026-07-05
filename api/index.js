28d9fd5d92840adc63408d3e63b4756b95ad147f
WiNDOWS964ourU <78335324+WiNDOWS964ourU@users.noreply.github.com>
2026-07-05 18:01:46 +0800

    Vercel Test-7

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import {handleRequest} from '../edge-functions/index.js'
 
 // for Vercel
-export default async function handler(request, env, context) {
-  return await handleRequest(request);
+export default async function handler(request) {
+  const response = await handleRequest(request);
+  return response;
 }
